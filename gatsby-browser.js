10451fc82d70b1fa38aaad10c92f8d91c7733169
Simon <contact@smn.codes>
2022-06-28 22:17:08 +0200

    v0.2.1: Oops, this should be in here.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,3 @@
-export const pluginOptionsSchema = ({ Joi }) => {
-  return Joi.object({
-    token: Joi.string().required(),
-  })
-}
-
 export const onRouteUpdate = (args, pluginOptions) => {
   const { token } = pluginOptions;
 
