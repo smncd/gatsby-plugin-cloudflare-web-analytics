19f4bc5d101d543a0a65f15b841c548855eb2c64
Simon <contact@smn.codes>
2022-07-03 13:58:37 +0200

    v0.3.0: Add option to add/overwrite script tag attributes

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,9 @@
 exports.pluginOptionsSchema = ({ Joi }) => {
   return Joi.object({
     token: Joi.string().required(),
+    attributes: Joi.array().items(Joi.object({
+      name: Joi.string().required(),
+      value: Joi.string().required(),
+  }))
   })
 }
