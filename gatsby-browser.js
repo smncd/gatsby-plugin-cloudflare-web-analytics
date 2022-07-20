cba456328bdc6c5f4e53bbab84eeebb002824dcc
Simon <contact@smn.codes>
2022-07-20 15:59:01 +0200

    v0.3.1: Check for undefined document

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,23 +1,26 @@
-exports.onRouteUpdate = (args, pluginOptions) => {
+exports.onRouteUpdate = (_, pluginOptions) => {
   const { token, attributes } = pluginOptions;
 
   if(!token || typeof token != 'string') {
     throw Error('gatsby-plugin-cloudflare-web-analytics: No token provided in gatsby-config.js');
   }
   
-  const head = document.head;
-  const script = document.createElement('script');
-  script.id = 'cloudflare-web-analytics'
-  script.defer = true;
-  script.src = 'https://static.cloudflareinsights.com/beacon.min.js';
-  script.setAttribute('data-cf-beacon', `{"token": "${token}"}`);
-
-  if(attributes) attributes.forEach(({name, value}) => script.setAttribute(name, value))
-
-  const existingScript = head.querySelector(`#${script.id}`);
-  if(existingScript) {
-    head.removeChild(existingScript)
+  if(document !== 'undefined') {
+    const head = document.head;
+    const script = document.createElement('script');
+    script.id = 'cloudflare-web-analytics'
+    script.defer = true;
+    script.src = 'https://static.cloudflareinsights.com/beacon.min.js';
+    script.setAttribute('data-cf-beacon', `{"token": "${token}"}`);
+  
+    if(attributes) attributes.forEach(({name, value}) => script.setAttribute(name, value))
+  
+    const existingScript = head.querySelector(`#${script.id}`);
+  
+    if(existingScript) {
+      head.removeChild(existingScript)
+    }
+  
+    head.appendChild(script);
   }
-
-  head.appendChild(script);
 }
